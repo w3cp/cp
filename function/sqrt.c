5f54ff90379768d0cf7d7aec02e9980033ac88f2
w3cp <jannat.books@gmail.com>
2016-02-11 02:55:16 +0000

    modify all

--- function/sqrt.c
+++ function/sqrt.c
@@ -1,55 +1,35 @@
-// 2015-08-17-20-09
 
 // to find out sqrt
 
-// MD ANWAR HOSSAIN
-// http://w3cp.blogspot.com
-
-// https://github.com/w3cp/
-// https://github.com/w3cp/cp/blob/master/function/sqrt.c
-
-
 #include <stdio.h>
 
-double mysqrt(double num);
+double mysqrt(double num); // to return the sqrt of its parameter
 
 int  main()
-
 {
+    double x;
+    
+    scanf("%lf", &x);
+    
+    printf("%0.2lf\n", mysqrt(x) );
+    
+    return 0;    
+} // end main
 
-    double x ;
-    
-    scanf("%lf" , &x);
-    
-    printf("%0.2lf\n" , mysqrt(x) );
-    
-    return 0;
-    
-}
-
+// returns the sqrt of its parameter
 double mysqrt(double number) {
-    
     double low, high, mid;
     
     low = 0.0;
-    
     high = number;
     
-    while (high - low > 0.000000001 ) {
-        
+    while (high - low > 0.000000001) {
         mid = (low + high) / 2;
-        
         if (mid * mid > number)
-        
             high = mid;
-            
-        else 
-        
+        else
             low = mid;
-        
     }
     
-    return mid;
-    
-}
-    
+    return mid;    
+} // end musqrt
